1ca1309e1308beba65493c3d1528ffe3ea91c7d3
gabrielmedsilva <gabrielmedsilva@outlook.com>
2025-03-19 14:09:26 -0300

    feat: Ajuste automático na escala da imagem

--- main.py
+++ main.py
@@ -2,7 +2,22 @@
 import cv2
 import pytesseract
 
-image = cv2.imread('.\images\print_vaga.png')
+image = cv2.imread(r'.\images\print_vaga.png')
+
+# Redimensionamento da imagem para padrão
+width = image.shape[1]
+height = image.shape[0]
+
+escale = float(height/width)
+resizedwidth = 1500
+resizedheight = int(resizedwidth*escale)
+
+image = cv2.resize(image, (resizedwidth, resizedheight), interpolation=cv2.INTER_AREA)
+
+# cv2.imshow('Image', image)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 tesseractPath = r"C:\Program Files\Tesseract-OCR"
 pytesseract.pytesseract.tesseract_cmd = tesseractPath + r"\tesseract.exe"
